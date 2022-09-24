afb1614297f377967d265ff57896a39f98f73e6f
Mkaycodes <101532260+Mkaycodes@users.noreply.github.com>
2022-09-24 01:14:35 +0000

    Update 0-add.py
    imports_and_modules_python3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
+
 #!/usr/bin/python3
-from add_0 import add
-if _name_ == '_main_':
+if __name__ == "__main__":
+    from add_0 import add
+
     a = 1
     b = 2
-    c = add(a, b)
-    print('{} ={} ={}'.format(a, b, c))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
